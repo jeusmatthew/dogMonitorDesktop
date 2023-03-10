ae58e9abbaf5aa849a3322eaaa6dc41fb3bc4b36
christian <christian.leon@deepdatat.com>
2023-03-10 01:24:14 -0600

    agrega colores

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const { app, BrowserWindow } = require('electron')
 
 const createWindow = () => {
     const win = new BrowserWindow({
-      width: 800,
-      height: 600
+      width: 900,
+      height: 900
     })
   
     win.loadFile('index.html')
