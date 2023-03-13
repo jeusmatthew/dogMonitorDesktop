f61d80e625593be8739e93156b757954b3135cc5
christian <christian.leon@deepdatat.com>
2023-03-13 01:30:48 -0600

    falta resolver problema de doble chart

--- chart.js
+++ chart.js
@@ -1,5 +1,5 @@
     const data = null;
-
+    
 if(data == null){
     const chart = document.getElementById("myChart");
     //chart.hidden = "hidden";
@@ -116,7 +116,175 @@ function readTextFile(file)
             if(rawFile.status === 200 || rawFile.status == 0)
             {
                 var allText = rawFile.responseText;
-                const 
+                const jsonData =  JSON.parse(allText);
+                let timeArray = jsonData.imu.map((log)=>log.sampled_at);
+                var unique = [];
+                timeArray.forEach(element => {
+                    if (!unique.includes(element)) {
+                        unique.push(element);
+                    }
+                });
+                const imuHeadData = jsonData.imu.filter((sample)=>sample.type == "head");
+                const imuTailData = jsonData.imu.filter((sample)=>sample.type == "tail");
+                const imuHeadXSamples = imuHeadData.map((sample) => sample.a_x);
+                const imuHeadYSamples = imuHeadData.map((sample) => sample.a_y);
+                const imuHeadZSamples = imuHeadData.map((sample) => sample.a_z);
+                const imuTailXSamples = imuTailData.map((sample) => sample.a_x);
+                const imuTailYSamples = imuTailData.map((sample) => sample.a_y);
+                const imuTailZSamples = imuTailData.map((sample) => sample.a_z);
+                var ctx = document.getElementById('myChart');
+                
+    console.log("setting chart");
+    var chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: unique,
+        datasets: [
+        {
+            label: 'Imu head acl X',
+            yAxisID: 'A',
+            borderColor: 'blue',
+            data:imuHeadXSamples,
+            fill: false
+        },
+        {
+            label: 'Imu head acl Y',
+            yAxisID: 'B',
+            borderColor: 'red',
+            data: imuHeadYSamples,
+            fill: false
+        },
+        {
+            label: 'Imu head acl z',
+            yAxisID: 'C',
+            borderColor: 'green',
+            data: imuHeadZSamples,
+            fill: false
+        },
+        {
+            label: 'Imu tail acl X',
+            yAxisID: 'D',
+            borderColor: 'pink',
+            data:imuTailXSamples,
+            fill: false
+        },
+        {
+            label: 'Imu tail acl Y',
+            yAxisID: 'E',
+            borderColor: 'yellow',
+            data: imuTailYSamples,
+            fill: false
+        },
+        {
+            label: 'Imu tail acl z',
+            yAxisID: 'F',
+            borderColor: 'gray',
+            data: imuTailZSamples,
+            fill: false
+        }
+
+     ]
+    },
+
+    // Configuration options go here
+    options: {
+        plugins:{
+            zoom:{
+                zoom:{
+                    enabled:true,
+                    wheel: {
+                        enabled: true,
+                    },
+                }
+            }
+        },
+        maintainAspectRatio:false,
+        responsive: true,
+        scales:{
+            yAxes: 
+            [
+                {
+                    id: 'A',
+                    type: 'linear',
+                    position: 'left',
+                    ticks:{
+                        max:2,
+                        min:-2,
+                        fontColor:'blue'
+                    },
+                    borderColor: 'blue',
+                },
+                {
+                    id: 'B',
+                    type: 'linear',
+                    position: 'left',
+                    borderColor: 'red',
+
+                ticks: {
+                    fontColor:"red",
+                    max: 2,
+                    min: -2
+                }
+                },
+                {
+                    // grid: {
+                    //     color: '#FF8000'
+                    // },
+                    id: 'C',
+                    type: 'linear',
+                    position: 'left',
+                    borderColor: 'green',
+
+                ticks: {
+                    fontColor:"green",
+                    max: 2,
+                    min: -2
+                }
+                },
+                {
+                    id: 'D',
+                    type: 'linear',
+                    position: 'left',
+                    ticks:{
+                        max:2,
+                        min:-2,
+                        fontColor:'pink'
+                    },
+                    borderColor: 'pink',
+                },
+                {
+                    id: 'E',
+                    type: 'linear',
+                    position: 'left',
+                    borderColor: 'yellow',
+
+                ticks: {
+                    fontColor:"yelow",
+                    max: 2,
+                    min: -2
+                }
+                },
+                {
+                    // grid: {
+                    //     color: '#FF8000'
+                    // },
+                    id: 'F',
+                    type: 'linear',
+                    position: 'left',
+                    borderColor: 'gray',
+
+                ticks: {
+                    fontColor:"gray",
+                    max: 2,
+                    min: -2
+                }
+                }
+            ]
+        },
+        
+    }
+});
+
             }
         }
     }
